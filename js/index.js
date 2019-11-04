6aecf6dd57dc13c1c305d44bc052d2c73035b516
Kristi <kgribble4083@gmail.com>
2019-11-03 21:16:08 -0600

    testing commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+// * [ ] `mouseover`
+let test = document.querySelectorAll(".nav-link");
+test.addEventListener("mouseover", (event) => {
+    event.target.style.color = "green";
+});
+
+    
+// * [ ] `keydown`
+
+// * [ ] `wheel`
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
