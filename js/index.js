f28c78206d0b4037ee4f229150f0fe490810a727
Kristi <kgribble4083@gmail.com>
2019-11-04 06:59:32 -0600

    added more event listener events dblclick and keydown

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-// Your code goes here
-// * [ ] `mouseover`
+
+// * [1] `mouseover`
 let funBus = document.querySelectorAll(".nav-link");
 
 
@@ -9,14 +9,14 @@ funBus.forEach(item => {
     });
 });
 
-// * [ ] `mouseout`
+// * [2] `mouseout`
 funBus.forEach(item => {
     item.addEventListener("mouseout", () => {
         item.style.color = "";
     });
 });
 
-// * [ ] `mouseenter`
+// * [3] `mouseenter`
 funBus.forEach(item => {
     item.addEventListener("mouseenter", () => {
         item.style.transform = "scale(1.5)";
@@ -24,7 +24,7 @@ funBus.forEach(item => {
     });
 });
 
-// * [ ] `mouseleave`
+// * [4] `mouseleave`
 funBus.forEach(item => {
     item.addEventListener("mouseleave", () => {
         item.style.transform = "";
@@ -32,19 +32,44 @@ funBus.forEach(item => {
     });
 });
 
-// * [ ] `load`
+// * [5] `resize`
+const moreFunBus = document.querySelector(".intro img");
+window.addEventListener("resize", () => {
+    moreFunBus.src = "img/destination.jpg";
+   
+});
+
+// * [6] `click`
+const button = document.querySelectorAll(".btn");
+button.forEach(item => {
+    item.addEventListener("click", () => {
+        item.style.backgroundColor = "green";
+        item.style.color = "white";
+
+    });
+});
+
+// * [7] `dblclick`
+button.forEach(item => {
+    item.addEventListener("dblclick", (event) => {
+        item.style.backgroundColor = "";
+        item.style.color = "";
+        event.preventDefault();
+    });
+});
+
+// * [8] `keydown`
+const keyDown = document.addEventListener("keydown", logKey);
+function logKey(key) {
+    alert(`You pressed the ${key.key} key! Was this a mistake?`);
+
+};
 
 
-// * [ ] `focus`
+// * [9]
 
 
-// * [ ] `resize`
+// * [10]
 
 
-// * [ ] `scroll`
 
-
-// * [ ] `select`
-
-
-// * [ ] `dblclick`
